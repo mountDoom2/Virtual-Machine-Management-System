592a62eaef9c21c7d07943b5e6b4ba7f843d607c
mountDoom2 <mountdoom@centrum.cz>
2016-05-17 23:46:46 +0200

    commentary

--- modules/errors.py
+++ modules/errors.py
@@ -1,4 +1,14 @@
+"""
+File: errors.py
+Author: Milan Skala
+Date: 2016-03-20
+Brief: Additional exceptions definitions 
+"""
+
 class EnvironmentException(Exception):
+    """
+    Raises when there is error during Environment initialization
+    """
     def __init__(self, message=""):
         super(Exception, self).__init__(message)
 
--- modules/globals.py
+++ modules/globals.py
@@ -1,4 +1,10 @@
+"""
+File: globals.py
+Author: Milan Skala
+Date: 2016-03-20
+Brief: Definition of global variables 
+"""
+
 historySupport = False
 cmdCompleter = False
-
 maxHistoryLen = 100
